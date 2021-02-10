8f6b2f9a1529469a3129861b7627ec4db17a87da
jennie6 <jenniechung6@gmail.com>
2021-02-09 17:52:37 -0700

    Getting type error

--- psets/set5/P3CHelpers.py
+++ psets/set5/P3CHelpers.py
@@ -46,15 +46,17 @@ def load_word_list(filename):
     Loads a list of the words from the url, removing all
     non-alpha-numeric characters from the file.
     """
-    with open(filename, 'r') as handle:
-        # Load a list of whitespace-delimited words from the specified file
-        raw_text = handle.read().strip().split()
-        # Strip non-alphanumeric characters from each word
-        alphanumeric_words = map(lambda word: ''.join(char for char in word if char.isalnum()), raw_text)
-        # Filter out words that are now empty (e.g. strings that only contained non-alphanumeric chars)
-        alphanumeric_words = filter(lambda word: len(word) > 0, alphanumeric_words)
-        # Convert each word to lowercase and return the result
-        return list(map(lambda word: word.lower(), alphanumeric_words))
+    handle = open('data.txt', 'r')
+    # Load a list of whitespace-delimited words from the specified file
+    raw_text = handle.read().strip().split()
+    # Strip non-alphanumeric characters from each word
+    alphanumeric_words = map(lambda word: ''.join(char for char in word if char.isalnum()), raw_text)
+    # Filter out words that are now empty (e.g. strings that only contained non-alphanumeric chars)
+    alphanumeric_words = filter(lambda word: len(word) > 0, alphanumeric_words)
+    # Convert each word to lowercase and return the result
+
+    handle.close()
+    return list(map(lambda word: word.lower(), alphanumeric_words))
 
 
 def generate_onehot_dict(word_list):
